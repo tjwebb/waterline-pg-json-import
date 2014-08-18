10b15ac6cb1576cc16afcabf5870fa8126e752ee
Travis Webb <me@traviswebb.com>
2014-08-17 23:28:30 -0400

    switch to sails-memory adapter; tests run much faster

--- test.js
+++ test.js
@@ -9,44 +9,45 @@ describe('waterline-pg-json-import', function () {
   var json = require('./build/postbooks_demo_460');
   var configuration = {
     adapters: {
-      disk: require('sails-disk')
+      memory: require('sails-memory')
     },
     connections: {
       mocha: {
-        adapter: 'disk'
+        adapter: 'memory'
       }
     }
   };
-  var orm, waterline;
+  var orm, waterline, collections;
 
   describe('#toORM', function () {
-    before(function () {
+    it('should run without error', function () {
       orm = importer.toORM(json, 'mocha');
-      fs.writeFileSync('./build/output.json', JSON.stringify(orm, null, 2));
+    });
+    it('result is accepted by waterline.loadCollection without error', function () {
       waterline = new Waterline();
-
       _.each(orm, waterline.loadCollection, waterline);
+      fs.writeFileSync('./build/output.json', JSON.stringify(orm, null, 2));
     });
 
     describe('Waterline#initialize', function () {
       this.timeout(120 * 1000);
-      var collections;
-      before(function (done) {
+
+      it('should run without error', function (done) {
         waterline.initialize(configuration, function (err, orm) {
           collections = orm.collections;
 
           done(err);
         });
       });
+    });
+  });
 
-      it('can create empty model', function (done) {
-        collections.accnt.create({ })
-          .then(function (accnt) {
-            done();
-          })
-          .catch(done);
-      });
-
+  describe('Waterline Models', function () {
+    it('can create empty model', function (done) {
+      collections.accnt.create({ })
+        .then(function (accnt) {
+          done();
+        }).catch(done);
     });
 
   });
